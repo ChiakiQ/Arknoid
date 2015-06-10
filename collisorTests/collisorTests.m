0f2c259f09e7f0c2687c8e3635eeb5f9ff553de2
ChiakiQ <348852310@qq.com>
2015-06-10 16:56:13 +0800

    oc

---
+++ collisorTests/collisorTests.m
@@ -0,0 +1,34 @@
+//
+//  collisorTests.m
+//  collisorTests
+//
+//  Created by OurEDA on 14-3-23.
+//  Copyright (c) 2014年 OurEDA. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface collisorTests : XCTestCase
+
+@end
+
+@implementation collisorTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
